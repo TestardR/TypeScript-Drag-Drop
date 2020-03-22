5cd84f74ac741316137875030a8ad3e26d68adbc
unknown <rmtestard@gmail.com>
2020-03-22 11:40:03 +0100

    I have added decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
+// autobind decorator
+function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
@@ -13,10 +33,35 @@ class ProjectInput {
       this.templateElement.content,
       true
     );
-    console.log(importedNode);
+
     this.formElement = <HTMLFormElement>importedNode.firstElementChild;
+    this.formElement.id = 'user-input';
+
+    this.titleInputElement = <HTMLInputElement>(
+      this.formElement.querySelector('#title')
+    );
+    this.descriptionInputElement = <HTMLInputElement>(
+      this.formElement.querySelector('#description')
+    );
+    this.peopleInputElement = <HTMLInputElement>(
+      this.formElement.querySelector('#people')
+    );
+
+    this.configure();
     this.attach();
   }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    // this.formElement.addEventListener('submit', this.submitHandler.bind(this));
+    this.formElement.addEventListener('submit', this.submitHandler);
+  }
+
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
   }
