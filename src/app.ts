861b373b05d3edb78f0e228e467a8e1b56f482e7
unknown <rmtestard@gmail.com>
2020-03-22 10:30:49 +0100

    1st commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement : HTMLDivElement;
+
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
+        this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+    }
+}
